0be5f7bae4c921b2f35c6fc19d90422cd82342dc
Phonggampanart Kaewta <basphong2542@gmail.com>
2020-03-06 15:57:42 +0700

    final edit

--- delete_customer.php
+++ delete_customer.php
@@ -11,27 +11,24 @@ $dbname = 'ionicapp';
 $connect = new mysqli($servername, $username, $password, $dbname);
 $connect->set_charset('utf8');
 
-if($connect->connect_error){
-    die("การเชื่อมต่อผิดพลาด  :".$connect->connect_error);
+if ($connect->connect_error) {
+    die("การเชื่อมต่อผิดพลาด : " . $connect->connect_error);
 }
 
+$id = $_GET["id"];
 
+$sql = "delete from customers where id = $id";
 
+// $result = $connect->query($sql);
 
-    $id = $_GET["id"];
+if ($connect->query($sql) === true) {
+    $connect->close();
+    return true;
 
-
-    $sql = "delete from customers where id = $id";
-
-    if($connect->query($sql) === TRUE){
-        $connect->close();
-        return true;
-    } else {
-        $connect->close();
-        return $connect->error;
-    }
-
+} else {
+    $connect->close();
+    return $connect->error;
 
     $connect->close();
 
-?>
+}
--- get_customer.php
+++ get_customer.php
@@ -10,23 +10,20 @@ $dbname = 'ionicapp';
 $connect = new mysqli($servername, $username, $password, $dbname);
 $connect->set_charset('utf8');
 
-if($connect->connect_error){
-    die("การเชื่อมต่อผิดพลาด  :".$connect->connect_error);
+if ($connect->connect_error) {
+    die("การเชื่อมต่อผิดพลาด : " . $connect->connect_error);
 }
 
 $sql = "select * from customers";
 $result = $connect->query($sql);
 
-if($result->num_rows > 0){
-   while($row = $result->fetch_assoc()){
-       $data[] = $row;
-   }
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
     echo json_encode($data);
-
-}else{
+} else {
     echo "ไม่มีข้อมูล";
 }
-
 $connect->close();
-
 ?>
